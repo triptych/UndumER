eb9a0ce67f646bf40269e6ff7df505b4cd0347f3
Andrew Wooldridge <triptych@gmail.com>
2011-06-22 17:21:29 -0700

    First commit of initial UI

---
+++ src/assets/js/undumer.js
@@ -0,0 +1,38 @@
+/** generate background noise 
+  * http://net.tutsplus.com/tutorials/javascript-ajax/how-to-generate-noise-with-canvas/ 
+  * **/ 
+  
+function generateNoise(opacity) {  
+   if ( !!!document.createElement('canvas').getContext ) {  
+      return false;  
+   }  
+  
+   var canvas = document.createElement("canvas"),  
+   ctx = canvas.getContext('2d'),  
+   x, y,  
+   number,  
+   opacity = opacity || 0.2;  
+  
+   canvas.width = 45;  
+   canvas.height = 45;  
+  
+   for ( x = 0; x < canvas.width; x++ ) {  
+      for ( y = 0; y < canvas.height; y++ ) {  
+         number = Math.floor( Math.random() * 60 );  
+  
+         ctx.fillStyle = "rgba(" + number + "," + number + "," + number + "," + opacity + ")";  
+         ctx.fillRect(x, y, 1, 1);  
+      }  
+   }  
+  
+   document.body.style.backgroundImage = "url(" + canvas.toDataURL("image/png") + ")";  
+}  
+ 
+  
+
+
+YUI().use("node", "event", function(Y){
+    Y.on("domready", function(Y){
+        generateNoise(0.1); // default opacity is .2 
+    })
+})
