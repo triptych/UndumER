3132294e48389499bbc7eaaba25da429d05431d1
Andrew Wooldridge <triptych@whisperstorm.com>
2011-06-28 22:45:03 +0000

    stuff

---
+++ src/assets/templates/html.js
@@ -0,0 +1,16 @@
+/** html template for substitution **/
+/** convert this to YUI.add **/
+
+
+
+udrtemp = {
+ html: 
+"<!DOCTYPE HTML>"+
+"<html lang='en'>"+
+"<head>"+
+"<meta charset='utf-8'>"+
+"<title>{title} by {author}</title>"+
+"<link href='http://fonts.googleapis.com/css?family=Tangerine' rel='stylesheet' type='text/css'>"+
+"</head>"+
+"</html>"
+};
